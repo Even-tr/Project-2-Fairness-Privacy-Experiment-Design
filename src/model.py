686119e622b964fdca4bc91d8c2234af5ddd2959
Even Tronstad <even@Evens-MacBook-Air.local>
2021-11-06 16:14:02 +0100

    Added comments tp current notebook, and added our first report

--- src/model.py
+++ src/model.py
@@ -11,9 +11,15 @@ class model:
 
     def update(self, features, actions, outcomes):
         for index, v in enumerate(self.a):
-            self.a[index] += np.sum(outcomes[np.where(actions == index)], axis=1)
-            self.b[index] += np.sum(outcomes[np.where(actions == index)]==0, axis=1)\
-                 - np.sum(outcomes[np.where(actions == index)], axis=1)
+
+            if np.sum(outcomes[np.where(actions == index)], axis=1).size != 0:
+                self.a[index] += np.sum(outcomes[np.where(actions == index)], axis=1)
+                    
+                self.b[index] += np.sum(outcomes[np.where(actions == index)]==0, axis=1)\
+                    - np.sum(outcomes[np.where(actions == index)], axis=1)
+            else:
+                self.b[index] += np.sum(outcomes[np.where(actions == index)]==0, axis=1)
+
 
     def get_prob(self, features, action):
         return self.a[action] / (self.a[action] + self.b[action])
